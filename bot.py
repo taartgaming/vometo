64dec62977d8a1c43f6997a39f44a59e7fc87b20
taartgaming <coolhero2000@gmail.com>
2021-07-03 17:28:38 +0200

    Anime dingdong

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import requests
+import json
+
+
 
 bot = commands.Bot(command_prefix='?')
 load_dotenv()
@@ -10,13 +14,21 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
 @bot.command()
 async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+
+@bot.command()
+async def animegif(ctx, arg):
+    await ctx.send(arg)
+    animeJSON = requests.get("https://api.giphy.com/v1/gifs/random?api_key=" + os.getenv('giphyToken') + "&tag=anime&rating=pg").json()
+    print(animeJSON["data"]["embed_url"])
+
     
 
 
